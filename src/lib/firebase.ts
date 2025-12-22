8c62f97f8b3282519e728680e9b755848d1a52a8
Naitik Jain <naitikjainjbp@gmail.com>
2025-12-22 16:30:22 +0000

    Try fixing this error: `Console Error: [2025-12-22T16:29:12.879Z] @fireb

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,9 +2,6 @@ import { initializeApp, getApps } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  // --- IMPORTANT ---
-  // Replace this with your own Firebase project configuration.
-  // You can find this in your Firebase project settings.
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
